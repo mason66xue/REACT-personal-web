9d935aab6e38712ffc3625c67025dcd961cf8187
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-02-03 17:18:19 -0500

    update header and nav

---
+++ src/component/Header/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Nav from "../Nav";
+
+function Header(props) {
+  const { currentTab, handleTabChange } = props;
+
+  return (
+    <div>
+      <section>
+        <header className="flex-row px-1">
+          <div>
+            <h1>Ruochen Liu's Portfolio</h1>
+          </div>
+          <div>
+            <Nav
+              currentTab={currentTab}
+              handleTabChange={handleTabChange}
+            ></Nav>
+          </div>
+        </header>
+      </section>
+      <section class="hero">
+        <div class="hero-cta">
+          {/* <img
+            src={coverImage}
+            className="my-2"
+            style={{ width: "100%" }}
+            alt="cover"
+          /> */}
+
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Header;
